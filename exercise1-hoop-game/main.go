ac4e5205f6a0248450b41acacea20e9713532160
mohammadsaleh.govahi <mohammadsaleh.govahi@partsoftware.com>
2024-09-15 18:02:33 +0330

    Upload answer of exercise 1

---
+++ exercise1-hoop-game/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number1, number2 int
+
+	fmt.Scanf("%d %d",&number1,&number2)
+	
+	start := 1
+
+	for start <= number2 {
+		if start % number1 == 0 {
+			hoopCount := start / number1
+			for i := 0; i < hoopCount; i++ {
+				fmt.Print("hoop ")
+			}
+			fmt.Printf("\n")
+		} else {
+			fmt.Printf("%d\n", start)
+		}
+		start++
+	}
+}
